b374fbf39e37285fc47447d36c8053185a903e11
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-01-31 10:17:51 +0100

    test path variable. close #1.

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const express = require('express');
 const app = express();
 
 app.get(['/','/index'],function(request,response) {
-    response.send('<h1> Hello in our first Rest app </h1>')
+    response.send('<h1> Hello in our first Rest app </h1>');
 });
 
 app.post(['/','/index'],function(request,response) {
-    response.send('<h1> Hello in our first Rest app VIA POST </h1>')
+    response.send('<h1> Hello in our first Rest app VIA POST </h1>');
 });
 
+let quotes = {
+    'einstein': 'We cannot solve our problems with the same thinking we used when we created them.',
+    'tesla': 'The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane.'
+};
+
+app.get('/quotes/:name', (request,response) => {
+    response.send(quotes[request.params.name]);
+});
+
+app.get('/posts/:month/:year', (request,response) => {
+    response.send('posts of : ' + JSON.stringify(request.params));
+});
 app.listen(3000,function () {
     console.log('Listen on 3000');
 });
